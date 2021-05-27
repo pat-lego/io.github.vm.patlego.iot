a65cb485e4937838ff4f2e5eea805159a743ba29
Patrique Legault <patrique.legault@gmail.com>
2021-05-26 21:02:30 -0400

    Deploy to master

--- server/server.core/src/main/java/io/github/vm/patlego/iot/server/sensor/interceptor/AuthenticationInterceptor.java
+++ server/server.core/src/main/java/io/github/vm/patlego/iot/server/sensor/interceptor/AuthenticationInterceptor.java
@@ -1,17 +1,27 @@
 package io.github.vm.patlego.iot.server.sensor.interceptor;
 
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+
 import org.apache.cxf.configuration.security.AuthorizationPolicy;
+import org.apache.cxf.endpoint.Endpoint;
 import org.apache.cxf.interceptor.Fault;
+import org.apache.cxf.message.Exchange;
 import org.apache.cxf.message.Message;
 import org.apache.cxf.phase.AbstractPhaseInterceptor;
 import org.apache.cxf.phase.Phase;
+import org.apache.cxf.transport.Conduit;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AuthenticationInterceptor extends AbstractPhaseInterceptor<Message> {
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
     public AuthenticationInterceptor() {
         super(Phase.INVOKE);
-        //TODO Auto-generated constructor stub
     }
 
     @Override
@@ -19,10 +29,49 @@ public class AuthenticationInterceptor extends AbstractPhaseInterceptor<Message>
         AuthorizationPolicy policy = message.get(AuthorizationPolicy.class);
         // Check to see if the header is well set if not return and send a nice piss off message 
         if (policy == null) {
+            sendErrorResponse(message, HttpURLConnection.HTTP_UNAUTHORIZED);
             return;
         }
         
         
+    }
+
+    private void sendErrorResponse(Message message, int responseCode) {
+        Message outMessage = getOutMessage(message);
+        outMessage.put(Message.RESPONSE_CODE, responseCode);
+       
+        message.getInterceptorChain().abort();
+        try {
+            getConduit(message).prepare(outMessage);
+            close(outMessage);
+        } catch (IOException e) {
+            logger.warn(e.getMessage(), e);
+        }
+    }
+
+    private Message getOutMessage(Message inMessage) {
+        Exchange exchange = inMessage.getExchange();
+        Message outMessage = exchange.getOutMessage();
+        if (outMessage == null) {
+            Endpoint endpoint = exchange.get(Endpoint.class);
+            outMessage = endpoint.getBinding().createMessage();
+            exchange.setOutMessage(outMessage);
+        }
+        outMessage.putAll(inMessage);
+        return outMessage;
+    }
+
+    private Conduit getConduit(Message inMessage) throws IOException {
+        Exchange exchange = inMessage.getExchange();
+        Conduit conduit = exchange.getDestination().getBackChannel(inMessage);
+        exchange.setConduit(conduit);
+        return conduit;
+    }
+
+    private void close(Message outMessage) throws IOException {
+        OutputStream os = outMessage.getContent(OutputStream.class);
+        os.flush();
+        os.close();
     }
     
 }
